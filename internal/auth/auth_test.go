86859c02fd044959ae2afb8cd2cd6956643bb2e6
Harry Y. <harryyu02@gmail.com>
2026-04-01 13:30:04 -0400

    add more test, ci test add cover flag

--- internal/auth/auth_test.go
+++ internal/auth/auth_test.go
@@ -20,3 +20,34 @@ func TestGetAPIKeyBasic(t *testing.T) {
         t.Errorf("GetAPIKey(%v) = %s; expected %s\n", headings, apiKey, expected)
     }
 }
+
+func TestGetAPIKeyEmptyHeader(t *testing.T) {
+	headings := http.Header{}
+	expectedErr := ErrNoAuthHeaderIncluded
+
+	apiKey, err := GetAPIKey(headings)
+	if err != nil {
+		if err != expectedErr {
+			t.Errorf("ERROR: %v", err)
+		}
+		return
+	}
+
+	t.Errorf("GetAPIKey(%v) = %s; expected err = %v\n", headings, apiKey, expectedErr)
+}
+
+func TestGetAPIKeyMalformedHeader(t *testing.T) {
+	headings := http.Header{}
+	headings.Add("Authorization", "Foobar testapikey")
+	expectedErr := ErrMalformedAuthHeader
+
+	apiKey, err := GetAPIKey(headings)
+	if err != nil {
+		if err != expectedErr {
+			t.Errorf("ERROR: %v", err)
+		}
+		return
+	}
+
+	t.Errorf("GetAPIKey(%v) = %s; expected err = %v\n", headings, apiKey, expectedErr)
+}
